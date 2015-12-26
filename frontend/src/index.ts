f183503743a15016ee3f38d3c81200a166d787e1
Ilya Rezvov <rezvov.ilya@undev.ru>
2015-12-26 19:42:48 +0200

    add ground

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -2,6 +2,18 @@
 
 import * as MatterJS from 'matter-js'
 
+const {Engine, Bodies, Body} = MatterJS
+
 const engine = MatterJS.Engine.create(document.querySelector('.wrapper'))
+const ground = Bodies.rectangle(400, 500, 780, 40, { isStatic: true, friction: 1 })
+const boxy = Bodies.rectangle(300, 100, 50, 50);
+
+MatterJS.World.add(engine.world, [ground, boxy]);
+
+document.addEventListener('keydown', (ev: KeyboardEvent) => {
+  if (ev.keyCode === 68) {
+    Body.applyForce(boxy, boxy.position, { x: 1, y: 0 })
+  }
+}, false)
 
 MatterJS.Engine.run(engine)
